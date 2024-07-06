db1d32636dd8470f0713511e63f576582a86b838
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-07-06 15:51:31 -0500

    hope

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:appsol_final/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(estalogeado:true,rol:4));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
